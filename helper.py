6845be08987dcf8a59ca5a086d36e42ad95bacc7
DreKwasi <andrewsboateng137@gmail.com>
2022-12-18 00:28:32 +0000

    Removed Date As Headers

--- helper.py
+++ helper.py
@@ -5,11 +5,41 @@ from google_sheet import connect_main, pull_data
 import os
 
 
+def parse_posts(df, select):
+    st.subheader(select)
+    grouped_dict = {}
+
+    df = df[df["note_type"] == select]
+
+    if not df.empty:
+        date_ranges = list(df["date_created"].unique())
+        date_ranges.sort(reverse=True)
+        for date in date_ranges:
+            grouped_dict[date] = df[df["date_created"] == date]
+
+        for key in grouped_dict.keys():
+            format_date = dt.datetime.strptime(key, "%Y-%m-%d")
+
+            df = grouped_dict[key]
+            for _, row in df.iterrows():
+                st.write(f"##### **:blue[{row['title']}]**")
+                expander = st.expander("View Summary")
+                expander.markdown(
+                    f"""{row['body']} 
+                                \n [Access Full Resource]({row['link']})
+                                \n **:violet[Published By _{row['publisher']}_]** 
+                                \n Date Uploaded: {format_date.strftime("%d-%B-%Y")}"""
+                )
+    else:
+        st.info("Coming Soon! Toggle the Dropdown Menu On the Sidebar for More Subjects")
+
+
 def full_setup(page_header, filename, table_name):
     # Page settings
     st.set_page_config(page_title="Swift Notes", page_icon="book", layout="wide")
 
     st.header(page_header)
+
 
     st.sidebar.header("Toggle Options")
     select = st.sidebar.selectbox(
@@ -31,7 +61,7 @@ def full_setup(page_header, filename, table_name):
                 parse_posts(df=df, select=select)
             except KeyError:
                 st.subheader("Keep In Touch")
-                st.success("Resources will be Uploaded Soon")
+                st.info("Resources will be Uploaded Soon")
 
     st.sidebar.markdown(
         """
@@ -40,34 +70,6 @@ def full_setup(page_header, filename, table_name):
     Would Love to Hear Your Feedback and Contributions"""
     )
 
-def parse_posts(df, select):
-    st.subheader(select)
-    grouped_dict = {}
-
-    df = df[df["note_type"] == select]
-
-    if not df.empty:
-        date_ranges = list(df["date_created"].unique())
-        date_ranges.sort(reverse=True)
-        for date in date_ranges:
-            grouped_dict[date] = df[df["date_created"] == date]
-
-        for key in grouped_dict.keys():
-            format_date = dt.datetime.strptime(key, "%Y-%m-%d")
-            st.subheader(format_date.strftime("%d-%B-%Y"))
-
-            df = grouped_dict[key]
-            for _, row in df.iterrows():
-                expander = st.expander(row["title"])
-                expander.markdown(
-                    f"""{row['body']} 
-                                \n [Read More]({row['link']})
-                                \n **:violet[Published By _{row['publisher']}_]** """
-                )
-    else:
-        st.success("Coming Soon! Explore Our Subjects")
-
 
 if "__name__" == "__main__":
     full_setup()
-
