fb176998d27425606fdc1217fae87fe8e50e6f1c
DreKwasi <andrewsboateng137@gmail.com>
2022-12-18 00:28:55 +0000

    Included function for updating Google Sheets

--- google_sheet.py
+++ google_sheet.py
@@ -26,7 +26,14 @@ def connect_main():
 def pull_data(spread, sheetname, filename):
     worksheet = spread.worksheet(sheetname)
     records = worksheet.get_all_records(
-        expected_headers=["title", "body", "publisher", "link", "date_created", "note_type"]
+        expected_headers=[
+            "title",
+            "body",
+            "publisher",
+            "link",
+            "date_created",
+            "note_type",
+        ]
     )
     df = pd.DataFrame(records).loc[
         :, ["title", "body", "publisher", "link", "date_created", "note_type"]
@@ -36,4 +43,22 @@ def pull_data(spread, sheetname, filename):
     df.to_csv(f"assets/{filename}/data/{filename}.csv", index=False)
     return df
 
-# def update_data(spread, sheetname, filename):
+def next_available_row(worksheet):
+    str_list = list(filter(None, worksheet.col_values(1)))
+    return str(len(str_list)+1)
+
+def update_data(spread, table_name, values_dict):
+    worksheet = spread.worksheet(table_name)
+    last_row = next_available_row(worksheet)
+
+    worksheet.update(
+        f"A{last_row}",
+        [[
+            values_dict["title"],
+            values_dict["body"],
+            values_dict["publisher"],
+            values_dict["link"],
+            values_dict["date"],
+            values_dict["info_type"],
+        ]]
+    )
